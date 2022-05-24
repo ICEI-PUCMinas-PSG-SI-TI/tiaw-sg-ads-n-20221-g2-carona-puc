0c7f552d07e161e146db4ca4343fe5d5d83ef5ed
rafaelafg <rafaela2003fg@gmail.com>
2022-05-24 20:17:07 -0300

    cod_rafa

---
+++ codigo/db.js
@@ -0,0 +1,21 @@
+/*Rafaela Ferreira Guimarães*/
+
+var db = {
+    dados: [
+        {
+            nome: 'João Melo',
+            curso: 'Psicologia',
+            turno: 'Noite',
+            genero: 'Maculino',
+            idade: '22',
+            whatsapp: '31 99999999',
+            email: 'joaomm@gmail.com',
+            bairro: 'Boa Vista',
+            unidade: 'São Gabriel',
+            carro: 'Honda Civic',
+            placa: 'KTJ-1010',
+            cor: 'Preto',
+            dias: 'S,T,Q,Q'
+        }
+    ]
+}
--- codigo/filtro.js
+++ codigo/filtro.js
@@ -1,3 +1,4 @@
+/*Rafaela Ferreira Guimarães*/
 function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
   }
@@ -17,3 +18,65 @@ function myFunction() {
       }
     }
   }
+
+  function myFunction2() {
+    document.getElementById("myDropdown2").classList.toggle("show");
+  }
+  
+  function filterFunction2() {
+    var input, filter, ul, li, a, i;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    div = document.getElementById("myDropdown2");
+    a = div.getElementsByTagName("a");
+    for (i = 0; i < a.length; i++) {
+      txtValue = a[i].textContent || a[i].innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        a[i].style.display = "";
+      } else {
+        a[i].style.display = "none";
+      }
+    }
+  }
+
+
+  function myFunction3() {
+    document.getElementById("myDropdown3").classList.toggle("show");
+  }
+  
+  function filterFunction3() {
+    var input, filter, ul, li, a, i;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    div = document.getElementById("myDropdown3");
+    a = div.getElementsByTagName("a");
+    for (i = 0; i < a.length; i++) {
+      txtValue = a[i].textContent || a[i].innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        a[i].style.display = "";
+      } else {
+        a[i].style.display = "none";
+      }
+    }
+  }
+  
+  function myFunction4() {
+    document.getElementById("myDropdown4").classList.toggle("show");
+  }
+  
+  function filterFunction4() {
+    var input, filter, ul, li, a, i;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    div = document.getElementById("myDropdown4");
+    a = div.getElementsByTagName("a");
+    for (i = 0; i < a.length; i++) {
+      txtValue = a[i].textContent || a[i].innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        a[i].style.display = "";
+      } else {
+        a[i].style.display = "none";
+      }
+    }
+  }
+
